a31f51d299c6b4743785465fed1ed2e8944af571
KittyMew0 <90781550+KittyMew0@users.noreply.github.com>
2023-12-22 09:37:34 +0300

    Add files via upload

---
+++ zadaniya6/zadanie1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива. */
+
+char[,] array = new char[,] { { '1', '2', '3'}, { 'a', 'b', 'c' } };
+string ending = Changing();
+Console.WriteLine(ending);
+
+string Changing() {
+    string ending = "";
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            ending = ending + array[i, j];
+        }
+    }
+    return ending;
+}
---
+++ zadaniya6/zadanie3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом. */
+
+string input = "lalal";
+int answer = checking(input);
+//Console.WriteLine("answer = " + answer);
+if (answer == 1) {
+    Console.WriteLine("yes");
+}
+else {
+    Console.WriteLine("no");
+}
+
+int checking(string input) {
+    string sinput = input;
+    char[] sReverse = sinput.ToCharArray();
+    Console.WriteLine(input + sinput);
+    Array.Reverse(sReverse);
+    sinput = new string(sReverse);
+    
+    if (input.Equals(sinput)) {
+        answer = 1;
+    }
+    else {
+        answer = 0;
+    }
+    
+    //Console.WriteLine("answer = " + answer);
+    return answer;
+}
